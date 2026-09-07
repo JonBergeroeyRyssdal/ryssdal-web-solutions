9f1680c4a7401b37f4fd275f9eaf03cccf86c4ae
Jon Bergerøy Ryssdal <jon@ryssdal.net>
2026-09-07 21:56:32 +0200

    portett

--- ryssdal-web-solutions/src/components/home/About.tsx
+++ ryssdal-web-solutions/src/components/home/About.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useLanguage } from "@/components/LanguageProvider";
+import Image from "next/image";
+import portrait from "../../../public/jon-ryssdal.png";
 import Section from "@/components/ui/Section";
 import SectionHeading from "@/components/ui/SectionHeading";
 
@@ -9,10 +11,17 @@ export default function About() {
   return (
     <Section id="om" className="about-section section-space" headingId="about-heading">
       <div className="row g-4 align-items-center">
-        <div className="col-lg-5">
-          <SectionHeading id="about-heading" eyebrow={t.about}>{t.aboutTitle}</SectionHeading>
+        <div className="col-md-4">
+          <Image
+            src={portrait}
+            alt="Jon Bergerøy Ryssdal"
+            className="about-portrait"
+            sizes="(max-width: 767px) 240px, 320px"
+            placeholder="blur"
+          />
         </div>
-        <div className="col-lg-7">
+        <div className="col-md-8">
+          <SectionHeading id="about-heading" eyebrow={t.about}>{t.aboutTitle}</SectionHeading>
           <p>{t.aboutText}</p>
           <p className="mb-0">{t.aboutProcess}</p>
         </div>
